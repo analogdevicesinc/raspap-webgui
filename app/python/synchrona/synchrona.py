5a62de2c0d395d5a72845431e6d38ce3c8c77d4b
Nuno Sá <nuno.sa@analog.com>
2022-04-13 16:30:07 +0200

    app: synchrona: fix vcxo selection
    We need to select the proper vcxo before we reload the overlay so that
    wen the devices are probing they already have the correct signal.
    Otherwise, the hmc7044 device initializes with it's PLLs unlocked.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>

--- app/python/synchrona/synchrona.py
+++ app/python/synchrona/synchrona.py
@@ -455,10 +455,12 @@ def configure_synchrona(config):
         return config
 
     ad9545_config(config)
-    subprocess.call("/var/www/html/app/python/synchrona/reload_dtb.sh")
+
     if config.vcxo == 100000000:
         subprocess.call(["/var/www/html/app/python/synchrona/set_vcxo_pin.sh", "0"])
     else:
         subprocess.call(["/var/www/html/app/python/synchrona/set_vcxo_pin.sh", "1"])
+
+    subprocess.call("/var/www/html/app/python/synchrona/reload_dtb.sh")
 
     return config
